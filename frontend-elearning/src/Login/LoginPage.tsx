d549a87874c270daad6dec00770347ae91713b3e
Capanu Andrei-Claudiu <andrei98ford@yahoo.com>
2022-06-15 12:59:42 +0300

    Login + Register Done!

--- frontend-elearning/src/Login/LoginPage.tsx
+++ frontend-elearning/src/Login/LoginPage.tsx
@@ -1,19 +1,89 @@
 import Button from '../Button/Button';
 import './LoginPage.css';
+import  { ChangeEvent, useState } from "react";
 type Props = {}
+type UserLoginCred = {
+  email:string;
+  password:string;
+}
 
 export default function LoginPage({}: Props) {
+
+
+
+
+  const[userInput, setInput] = useState({
+    email:"",
+    password:"",
+  });
+
+  function flushForm() {
+    setInput({
+      email:"",
+      password:"",
+    });
+    (document.getElementById("email") as HTMLInputElement).value="";
+    (document.getElementById("password") as HTMLInputElement).value="";
+  }
+
+
+  const handleChange = (event:ChangeEvent<HTMLInputElement>|ChangeEvent<HTMLSelectElement>) => { 
+    setInput((prevState:UserLoginCred) => ({
+      ...prevState,
+      [(event.target as HTMLInputElement).id]:(event.target as HTMLInputElement).value,
+    }));
+  };
+
+  function isEmailValid(email:string){
+    const regex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    if (!String(email).toLowerCase().match(regex)) {
+       alert("Format of your email is not valid!");
+       return false
+    } else if(email.length<6 || email.length>40) {
+       alert("Your email must have between 6 and 40 characters!");
+       return false;
+    }
+    return true;
+  }
+
+  function isPasswordValid(password:string){
+      
+    if(password.length<5 || password.length>20) {
+        alert("Your password must have between 5 and 20 characters!");
+        return false;
+    }
+      return true;
+  }
+  
+  function isUserInpuValid(){
+    let ok:boolean;
+    ok = false;
+
+    ok = isEmailValid(userInput.email);
+    if (ok === true) {
+      ok = isPasswordValid(userInput.password);
+    }
+    
+    return ok;
+  }
+  const handleSubmit = () =>{
+      console.log(userInput);
+      if (isUserInpuValid())
+        flushForm();
+  };
   return (
     <div className='login-page'>
        
         <div className='login-page__form'>
         <h1 className='login-page__title'>Sign in to your account!</h1>
             <label className='login-page__form__label' >Email</label>
-            <input  className="login-page__form__input-text" type="text" id="email"  placeholder="Your email is...." required/>
+            <input  onChange={handleChange}  className="login-page__form__input-text" type="text" id="email"  placeholder="Your email is...." required/>
             <label className='login-page__form__label' >Password</label>
-            <input  className="login-page__form__input-text" type="text" id="password"  placeholder="Your password is...." required/>
+            <input  onChange={handleChange}  className="login-page__form__input-text" type="password" id="password"  placeholder="Your password is...." required/>
             
-            <Button text='Sign in' func={()=>{console.log("sign in")}}></Button>
+            <Button text='Sign in' func={()=>{ handleSubmit()}}></Button>
         </div>
     </div>
   )
--- frontend-elearning/src/Register/RegisterPage.tsx
+++ frontend-elearning/src/Register/RegisterPage.tsx
@@ -22,6 +22,21 @@ export default function RegisterPage() {
     role:""
   });
 
+  function flushForm() {
+    setInput({
+      email:"",
+      password:"",
+      firstName:"",
+      lastName:"",
+      role:""
+    });
+
+    (document.getElementById("email") as HTMLInputElement).value="";
+    (document.getElementById("password") as HTMLInputElement).value="";
+    (document.getElementById("firstName") as HTMLInputElement).value="";
+    (document.getElementById("lastName") as HTMLInputElement).value="";
+    (document.getElementById("role") as HTMLInputElement).value="NONE";
+  }
   const handleChange = (event:ChangeEvent<HTMLInputElement>|ChangeEvent<HTMLSelectElement>) => { 
     setInput((prevState:UserRegisterCred) => ({
       ...prevState,
@@ -29,9 +44,79 @@ export default function RegisterPage() {
     }));
   };
 
-  // const handleSubmit = (event:ChangeEvent<HTMLButtonElement>) {
+  function isEmailValid(email:string){
+    const regex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    if (!String(email).toLowerCase().match(regex)) {
+       alert("Format of your email is not valid!");
+       return false
+    } else if(email.length<6 || email.length>40) {
+       alert("Your email must have between 6 and 40 characters!");
+       return false;
+    }
+    return true;
+  }
 
-  // }
+  function isPasswordValid(password:string){
+      
+    if(password.length<5 || password.length>20) {
+        alert("Your password must have between 5 and 20 characters!");
+        return false;
+    }
+      return true;
+  }
+  function isFirstNameValid(firstName:string){
+    if(firstName.length<3 || firstName.length>20) {
+      alert("Your first name must have between 3 and 20 characters!");
+      return false;
+    }
+    return true;
+  }
+
+  function isLastNameValid(lastName:string){
+    if(lastName.length<3 || lastName.length>20) {
+      alert("Your last name must have between 3 and 20 characters!");
+      return false;
+    }
+    return true;
+  }
+
+  function isRoleValid(role:string){
+    if(role===""  || role==="NONE") {
+      alert("Role filed is empty!");
+      return false;
+    }
+    return true;
+  }
+
+  function isUserInpuValid(){
+    let ok:boolean;
+    ok = false;
+
+    ok = isEmailValid(userInput.email);
+    if (ok === true) {
+      ok = isPasswordValid(userInput.password);
+    }
+    if (ok === true) {
+      ok = isFirstNameValid(userInput.firstName);
+    }
+    if (ok === true) {
+      ok = isLastNameValid(userInput.lastName);
+    }
+    if (ok === true) {
+      ok = isRoleValid(userInput.role);
+    }
+  
+    
+    return ok;
+  }
+  const handleSubmit = () =>{
+      console.log(userInput);
+      if (isUserInpuValid())
+        flushForm();
+  };
+
   return (
     <div className='register-page'>
        
@@ -40,18 +125,18 @@ export default function RegisterPage() {
             <label className='register-page__form__label' >Email</label>
             <input  onChange={handleChange} className="register-page__form__input-text" type="text" id="email"  placeholder="Your email is...." />
             <label className='register-page__form__label' >Password</label>
-            <input   onChange={handleChange} className="register-page__form__input-text" type="text" id="password"  placeholder="Your password is...." />
+            <input   onChange={handleChange} className="register-page__form__input-text" type="password" id="password"  placeholder="Your password is...." />
             <label className='register-page__form__label' >First Name</label>
             <input  onChange={handleChange} className="register-page__form__input-text" type="text" id="firstName"  placeholder="Your first name is...." />
             <label className='register-page__form__label' >Last Name</label>
             <input onChange={handleChange} className="register-page__form__input-text" type="text" id="lastName"  placeholder="Your last name is...." />
             <label className='register-page__form__label' >Role</label>
-            <select   onChange={handleChange} id="rol"  className="register-page__form__select" >
+            <select   onChange={handleChange} id="role"  className="register-page__form__select" >
                 <option value="NONE">Please select role</option>
                 <option value="TEACHER">Teacher</option>
                 <option value="STUDENT">Student</option>
             </select>
-            <Button text='Register!' func={()=>{ console.log("Click!")}}/>
+            <Button text='Register!' func={()=>{ handleSubmit()}}/>
         </div>
     </div>
   )
