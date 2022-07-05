48539e2d7a2a83fef49a3f9b4c401d4a7781d3cc
alexandru-w98 <stoica.alex.w98@gmail.com>
2022-07-05 12:26:52 +0300

    fixare sticky footer main

--- frontend-elearning/src/Footer/Footer.tsx
+++ frontend-elearning/src/Footer/Footer.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Button from '../Button/Button';
-import styles from './Footer.module.css';
+import React from "react";
+import Button from "../Button/Button";
+import styles from "./Footer.module.css";
 
 type Props = {
-    courseTitle?:string,
-    sectionTitle?:string,
-    quizzTitle?:string
-}
+  courseTitle?: string;
+  sectionTitle?: string;
+  quizzTitle?: string;
+};
 
 export default function Footer(props: Props) {
-    return (
-        <div className={`${styles["pageColor"]}`}>
-            <span className={`${styles["text--footer"]}`}>Learn now!</span>
-            <span className={`${styles["text--footer"]}`}>{props.courseTitle}</span>
-            <span className={`${styles["text--footer"]}`}>{props.sectionTitle}</span>
-            <span className={`${styles["text--footer"]}`}>{props.quizzTitle}</span>
-            <span className={`${styles["text--footer"]}`}>Have Fun!!</span>
-            <footer className={`${styles["text--footer"]}`}>
-			&copy; {new Date().getFullYear()} Copyright: Goal Diggers Team
-		</footer>
-        </div>
-    )
+  return (
+    <div className={`${styles["pageColor"]} ${styles["footer"]}`}>
+      <span className={`${styles["text--footer"]}`}>Learn now!</span>
+      <span className={`${styles["text--footer"]}`}>{props.courseTitle}</span>
+      <span className={`${styles["text--footer"]}`}>{props.sectionTitle}</span>
+      <span className={`${styles["text--footer"]}`}>{props.quizzTitle}</span>
+      <span className={`${styles["text--footer"]}`}>Have Fun!!</span>
+      <footer className={`${styles["text--footer"]}`}>
+        &copy; {new Date().getFullYear()} Copyright: Goal Diggers Team
+      </footer>
+    </div>
+  );
 }
